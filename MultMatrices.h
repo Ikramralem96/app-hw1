9171fb4861d2020d56ed7582074095295c3d7aa0
Ikramralem96 <Ikramralem96@gmail.com>
2021-01-25 22:03:38 +0100

    Add files via upload

---
+++ MultMatrices.h
@@ -0,0 +1,8 @@
+#ifndef MULTMATRICES_H_
+#define MULTMATRICES_H_
+
+
+void MultMatrices (int **M1 /*matrice carre 1*/ ,int **M2 /*matrice carre 2*/ ,int **R /*matrice de Resultat*/ , int size/*la taille de M1 et M2*/);
+
+
+#endif /* MULTMATRICES_H_ */
