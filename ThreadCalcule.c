36bbd10ebb9618d1e2f55f8c4ddf204bc0f72dc0
Ikramralem96 <Ikramralem96@gmail.com>
2021-02-09 21:22:24 +0100

    Update ThreadCalcule.c

--- ThreadCalcule.c
+++ ThreadCalcule.c
@@ -17,6 +17,22 @@ int instart;
 int iend;
 int istart;
 }blocksize;
+
+typedef struct blocksize_Matrix_Form{
+	int n;
+	int NbrThreads;
+	int taille;
+	int reste;
+	int istart;
+	int iend;
+	int jstart;
+	int jend;
+	int **A;
+	int **B;
+	int **C;
+}blocksize_Matrix_Form;
+
+
  
 void * MultipleMatrix(){
 	istart = i * 	
@@ -28,5 +44,15 @@ void * MultipleMatrix(){
 	}
 }
    return NULL;
+}
+void *Mult_vectorielle(void * args){		
+	blocksize * blk = (blocksize *) args;
+	for(int i = blk->istart; i<=blk->iend; i++){
+		for(int j=0; j<blk->n; j++)
+			for(int k=0; k<blk->n; k++){
+				blk->C[i][j] = blk->C[i][j] + blk->A[i][k] * blk->B[k][j];			
+			}
+		}
+	return NULL;
 }
 
